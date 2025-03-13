9b3489cb76f84b603eef2acde4cfbd6f4a450260
Galo <galoberlini@gmail.com>
2025-03-13 18:59:24 -0300

    Adición de metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,7 +10,20 @@ const ubuntuSans = Ubuntu_Sans({
 
 export const metadata: Metadata = {
   	title: "Galo Berlini Fullstack Developer",
- 	description: "Galo Berlini. Fullstack Developer",
+ 	description: "A personal portfolio developed with Next.js, showcasing my projects and skills as a web developer.",
+	 openGraph: {
+		title: 'Galo Berlini Fullstack Developer',
+		siteName: 'Galo Berlini Fullstack Developer',
+		description: 'A personal portfolio developed with Next.js, showcasing my projects and skills as a web developer.',
+		images: ['https://galo-berlini.vercel.app/images/PortfolioTools.png'],
+		url: 'https://galo-berlini.vercel.app/', 
+	  },
+	  twitter: {
+		card: 'summary_large_image',
+		title: 'Galo Berlini Fullstack Developer',
+		description: 'A personal portfolio developed with Next.js, showcasing my projects and skills as a web developer.',
+		images: ['https://galo-berlini.vercel.app/images/PortfolioTools.png'],
+	  },
 };
 export const viewport: Viewport = {
   	width: 'device-width',
